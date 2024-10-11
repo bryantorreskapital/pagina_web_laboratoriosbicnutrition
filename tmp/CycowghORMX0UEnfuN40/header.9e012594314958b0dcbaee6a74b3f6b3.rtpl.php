7ac30a42bb2b82215ba693b6d9e1a4d7d8cf3a58
dereck rodriguez <stalin.rodriguez8424@gmail.com>
2024-10-10 21:00:28 -0500

    Cambio diseño de productos y lo agrupo por categoria

--- tmp/CycowghORMX0UEnfuN40/header.9e012594314958b0dcbaee6a74b3f6b3.rtpl.php
+++ tmp/CycowghORMX0UEnfuN40/header.9e012594314958b0dcbaee6a74b3f6b3.rtpl.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="utf-8">
-    <title>KAPITALCOMPANY - Sistema Contable</title>
+    <title>BIC NUTRITION</title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta content="" name="keywords">
     <meta content="" name="description">
@@ -171,9 +171,9 @@
             </div>
         </div>
 
-        <nav class="navbar navbar-expand-lg navbar-light py-lg-0 px-lg-5 wow fadeIn" data-wow-delay="0.1s">
+        <nav class="navbar navbar-expand-lg navbar-light py-lg-0 px-lg-5 wow fadeIn" data-wow-delay="0.1s" style="background-color: rgb(140 203 59 / 92%); color: #fffff !important";">
             <a href="index.php?page=home" class="navbar-brand ms-2 ms-lg-0">
-                <h1 class="display-18 text-dark m-0" translate="no">LAB. BIC NUTRITION</h1>
+                <h1 class="display-18 text-dark m-0" translate="no">BIC NUTRITION</h1>
             </a>
             <!--<a href="index.php?page=home" class="navbar-brand ms-4 ms-lg-0">
               <svg width="400" height="100" viewBox="0 0 400 100">
